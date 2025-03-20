a52810227df73c64e123d558299cf389ef006ea7
abhinavsinha <abhinavvsinhaa@gmail.com>
2025-03-20 22:51:43 +0530

    added comments

--- coreJava/OOPS.java
+++ coreJava/OOPS.java
@@ -1,9 +1,12 @@
 package coreJava;
 
 // every class has two things - properties (variables), and behaviours (functions/methods)
+// every function has its own stack, for example "main" would have its own stack, and "add" would have of its own
 class Calculator {
+    int num = 10; // instance variable
+
     public int add(int a, int b) {
-        return a + b;
+        return a + b; // a, b -> local variables
     }
 
     public int add(int a, int b, int c) {
@@ -16,6 +19,8 @@ public class OOPS {
         int num1 = 5;
         int num2 = 10;
 
+        // cal is actually a reference variable, that stores the address of the object
+        // created inside heap
         Calculator cal = new Calculator();
         int result = cal.add(num1, num2);
         System.out.println(result);
